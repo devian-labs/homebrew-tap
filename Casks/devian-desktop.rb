ac7ab6e679b254fa8638e07ebddb5f79f09f2ab2
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2026-02-27 02:29:12 +0530

    chore: revert url suffix for aarch64 payloads

--- Casks/devian-desktop.rb
+++ Casks/devian-desktop.rb
@@ -2,7 +2,7 @@ cask "devian-desktop" do
   version "0.2.1"
   sha256 :no_check
 
-  url "https://github.com/devian-labs/devian-web/releases/download/v#{version}/Devian.Desktop_#{version}_universal.dmg"
+  url "https://github.com/devian-labs/devian-web/releases/download/v#{version}/Devian_Desktop_#{version.split('-').first}_aarch64.dmg"
   name "Devian"
   desc "Local AI Desktop Environment for Developers"
   homepage "https://github.com/devian-labs/devian-web"
