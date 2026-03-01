05b6ccb4368b6adf897e2b8d5ab68cbe98341426
GitHub Action <action@github.com>
2026-03-01 04:18:29 +0000

    chore: bump cask version to v1.0.0

--- Casks/devian-desktop.rb
+++ Casks/devian-desktop.rb
@@ -1,5 +1,5 @@
 cask "devian-desktop" do
-  version "0.2.2"
+  version "1.0.0"
   sha256 :no_check
 
   url "https://github.com/devian-labs/devian-web/releases/download/v#{version}/Devian.Desktop_#{version}_aarch64.dmg"
